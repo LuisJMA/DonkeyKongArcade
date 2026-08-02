4286a37bd9d0a4844615d96c303d2dfeeaaf85e6
LuisJMA <146795211+LuisJMA@users.noreply.github.com>
2026-08-02 18:01:58 -0400

    feat: actualiza GamePanel con plataformas y fisica

--- src/com/donkeykong/view/GamePanel.java
+++ src/com/donkeykong/view/GamePanel.java
@@ -1,6 +1,7 @@
 package com.donkeykong.view;
 
 import com.donkeykong.controller.GameController;
+import com.donkeykong.model.Platform;
 import com.donkeykong.model.Player;
 import javax.swing.JPanel;
 import javax.swing.Timer;
@@ -8,17 +9,27 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GamePanel extends JPanel implements ActionListener {
     private Timer gameTimer;
     private Player player;
     private GameController controller;
+    private List<Platform> platforms;
 
     public GamePanel() {
         setBackground(Color.BLACK);
         setFocusable(true);
 
-        player = new Player(100, 450);
+        // Inicializar plataformas del escenario
+        platforms = new ArrayList<>();
+        platforms.add(new Platform(100, 500, 600, 20)); // Suelo principal
+        platforms.add(new Platform(200, 380, 400, 20)); // Plataforma intermedia
+        platforms.add(new Platform(100, 260, 400, 20)); // Plataforma superior
+
+        // Inicializar jugador y controles
+        player = new Player(120, 440);
         controller = new GameController(player);
         addKeyListener(controller);
 
@@ -28,7 +39,7 @@ public class GamePanel extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        player.update();
+        player.update(platforms);
         repaint();
     }
 
@@ -37,8 +48,14 @@ public class GamePanel extends JPanel implements ActionListener {
         super.paintComponent(g);
         
         g.setColor(Color.WHITE);
-        g.drawString("Mueve al personaje con las flechas o W-A-S-D", 50, 50);
+        g.drawString("Usa A-D para moverte y W o Flecha Arriba para saltar", 50, 30);
         
+        // Dibujar plataformas
+        for (Platform p : platforms) {
+            p.draw(g);
+        }
+
+        // Dibujar jugador
         player.draw(g);
     }
 }
